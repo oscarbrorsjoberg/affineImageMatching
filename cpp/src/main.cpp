140ad95bd0eddcce5f3631b9918c49ffa92e60a9
Oscar Sjoberg <oscarbror.sjoberg@gmail.com>
2023-05-31 23:55:24 +0200

    inital run -- might not build atm

--- cpp/src/main.cpp
+++ cpp/src/main.cpp
@@ -1,3 +1,4 @@
+#include <cstdlib>
 #include <opencv2/core.hpp>
 #include <opencv2/imgproc.hpp>
 #include <opencv2/features2d.hpp>
@@ -6,15 +7,30 @@
 #include <opencv2/calib3d.hpp>
 
 #include <iostream>
+#include <fstream>
 #include <iomanip>
 #include <filesystem>
 
+static std::string exe_name(){
+  std::string out;
+  std::ifstream("/proc/self/comm") >> out;
+  return out;
+}
 
 int main(int argc, char *argv[])
 {
+  try {
+    if(argc < 3){
+      throw std::runtime_error("Missing input parameters, " +
+          exe_name() +
+          " <im1> <im2>");
+    }
 
-  std::cout << "Hello World\n";
-
+  }
+  catch (const std::exception &e) {
+    std::cerr << e.what() << std::endl;
+    return EXIT_FAILURE;
+  }
 
   return EXIT_SUCCESS;
 }
