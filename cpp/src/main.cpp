d60d25cef3c972108e61b0f625787801463942b3
Oscar Sjoberg <oscarbror.sjoberg@gmail.com>
2023-05-31 21:59:53 +0200

    inital commit for cpp code

---
+++ cpp/src/main.cpp
@@ -0,0 +1,21 @@
+#include <opencv2/core.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/features2d.hpp>
+#include <opencv2/xfeatures2d.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/calib3d.hpp>
+
+#include <iostream>
+#include <iomanip>
+#include <filesystem>
+
+
+int main(int argc, char *argv[])
+{
+
+  std::cout << "Hello World\n";
+
+
+  return EXIT_SUCCESS;
+}
+
